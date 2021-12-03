c7c4b2cdeec1d5213536a245c498db620de116c6
Kacper <kacper.791@o2.pl>
2021-12-04 00:04:30 +0100

    dividing cartPage

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,88 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Card from "../components/common/Card";
 import Text from "../components/common/Text";
 import Header from "../components/UI/Header/Header";
-import ResultItem from "../components/UI/SearchSide/Result/ResultItem";
 import beach from "../assets/beach.jpg";
-import axios from "axios";
-import { useSelector } from "react-redux";
 import PurchasingForm from "../components/UI/CartPage/PurchasingForm";
+import SummaryText from "../components/UI/CartPage/SummaryText";
 
 export default function CartPage(props) {
-  const hotels = useSelector((state) => state.hotel.hotels);
-  const route = useSelector((state) => state.route.routeHint);
-
-  const [selectedHotel, setSelectedHotel] = useState();
-  const [tripData, setTripData] = useState({
-    distance: 0,
-    price: 0,
-    duration: 0,
-  });
-
-  const calcPrice = (distanceValue, duration) => {
-    axios
-      .post("http://localhost:5000/ticket/price", {
-        distance: distanceValue,
-      })
-      .then((res) => {
-        setTripData({
-          price: res.data.ticketPrice,
-          distance: tripData.distance,
-          duration: duration,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const formatString = (text) => {
-    let result = 0;
-    let newText = text;
-    newText = newText.replace(/€/g, "");
-    newText = newText.split("-");
-
-    result = getMediumPrice(newText);
-    return result;
-  };
-  const getMediumPrice = (arr) => {
-    let result = arr.reduce((acc, curr) => parseInt(acc) + parseInt(curr));
-    return result / 2;
-  };
-
-  useEffect(() => {
-    let distance = 0;
-    let duration = 0;
-    if (hotels) {
-      let hotel = hotels.find((item) => {
-        return item.location_id === props.match.params.hotelId;
-      });
-      setSelectedHotel(hotel);
-    }
-
-    if (route) {
-      let distanceArr = [];
-      let timeArr = [];
-      route.forEach((element) => {
-        distanceArr.push(element.distanceValue);
-        timeArr.push(element.durationValue);
-      });
-      distance = distanceArr.reduce((acc, curr) => acc + curr);
-      duration = timeArr.reduce((acc, curr) => acc + curr);
-      setTripData({
-        distance: distance / 1000,
-        price: tripData.price,
-        duration: duration,
-      });
-      calcPrice(distance / 1000, (duration / 3600).toFixed(0));
-    }
-  }, [
-    setSelectedHotel,
-    selectedHotel,
-    hotels,
-    props.match.params.hotelId,
-    route,
-    tripData.price,
-  ]);
   return (
     <>
       <Header
@@ -129,61 +53,7 @@ export default function CartPage(props) {
               </Text>
             </Card>
           </Card>
-          <Card height="40%" width="100%" justifyContent="flex-start">
-            <Card width="60%">
-              {selectedHotel && (
-                <ResultItem
-                  hotel={selectedHotel}
-                  visible={false}
-                  width="100%"
-                  margin="0"
-                />
-              )}
-            </Card>
-            <Card
-              width="40%"
-              flexDirection="column"
-              justifyContent="flex-start"
-              overflow="scroll"
-            >
-              <Text
-                fontSize="26px"
-                color="white"
-                textAlign="center"
-                margin="1% 0 2% 0"
-                fontWeight="bold"
-              >
-                Congratulations of great choice <br />
-              </Text>
-              <Text fontSize="1.3em" color="white" margin="1% 5%">
-                Hotel {selectedHotel && selectedHotel.name} is one of the best
-                hotels in your destination
-              </Text>
-              <Text fontSize="1.3em" color="white" margin="1% 5%">
-                The price for staying in selected hotel for 1 adult for 5 night
-                costs around {selectedHotel && selectedHotel.price}
-              </Text>
-              <Text fontSize="1.3em" color="white" margin="1% 5%">
-                Using public communication transport you will pay approximately{" "}
-                {tripData.price && tripData.price}€. Transport is going to last
-                about {tripData.duration && tripData.duration} hours
-              </Text>
-              <Text
-                fontSize="1.8em"
-                color="white"
-                margin="1% 5%"
-                textAlign="center"
-                fontWeight="bold"
-              >
-                Summary
-              </Text>
-              <Text fontSize="1.3em" color="white" margin="1% 5%">
-                Total Price:{" "}
-                {selectedHotel && formatString(selectedHotel.price)}€ <br />
-                If you are committed to this offer, please fill the form below
-              </Text>
-            </Card>
-          </Card>
+          <SummaryText match={props.match} />
           <PurchasingForm />
         </Card>
       </Card>
